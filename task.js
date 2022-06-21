2e96af8fc06de0e434c6cfc16bf555027ebd75de
grkmtsn <grkkmm1@gmail.com>
2022-06-21 21:55:02 +0300

    add task case

---
+++ task.js
@@ -0,0 +1,7 @@
+// Q1. İçinde 5 tane sayı olan bir dizi tanımlayınız.
+// Q2. İleride kullanılmak üzere yukarıdaki dizideki sayıların 2 şer katını tutacak olan yeni boş bir dizi tanımlayınız.
+// Q3. Parametre olarak aldığı dizinin elemanı olup olmadığını kontrol eden ve bu kontrole göre bir boolean döndüren fonksiyon yazınız.
+// Q4. Yazdığınız bu fonksiyonu kullanarak yukarıda tanımladığınız dizinin elemanı olup olmadığını bir değişkende saklayınız.
+// Q5. Bu değişkeni kullanarak eğer dizinin elemanı varsa, bir döngü ile (for, for-of, foreach) bu diziyi dönüp her bir elemanı 2 şer katlarını tutacağımız diziye atınız.
+// Bonus: 5. adımı array sınıfının map methodunu kullanarak yapmaya çalışınız.
+// Not: Değişken isimleri önemli ve camelCase olmalı.
